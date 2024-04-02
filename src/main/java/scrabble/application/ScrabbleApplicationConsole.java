108bec44f04b68dc161e02095917d853a2474617
Louis Bec <louis.0505@protonmail.com>
2024-04-02 14:27:21 +0200

    add ScrabbleApplicationConsole

---
+++ src/main/java/scrabble/application/ScrabbleApplicationConsole.java
@@ -0,0 +1,14 @@
+package scrabble.application;
+
+
+public class ScrabbleApplicationConsole {
+	private static String SEPARATOR = "---------------------------------------------------------";
+
+	public static void main(String[] args) {
+		System.out.println(SEPARATOR);
+		System.out.println("--  Bienvenue dans notre magnifique jeu de Scrabble!  -- ");
+		System.out.println(SEPARATOR);
+
+	}
+
+}

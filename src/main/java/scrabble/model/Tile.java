7f0632819bf3ed4da7dcc957b3c12c8eac4939f2
Evan Gerbeaud <gerbeaudevan@gmail.com>
2024-04-08 11:16:45 +0200

    add Tile.java and Letter.java

---
+++ src/main/java/scrabble/model/Tile.java
@@ -0,0 +1,17 @@
+package scrabble.model;
+
+public class Tile {
+
+	private final Letters letter;
+	
+	
+	public Letters getLetter() {
+		return letter;
+	}
+
+
+	public Tile(Letters letter) {
+		this.letter = letter;
+	}
+
+}

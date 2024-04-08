87a413795920242f6bea6121fb191d7b0357bfe1
Louis Bec <louis.0505@protonmail.com>
2024-04-08 11:31:20 +0200

    add Square.java SquareStar.java and GameBoard.java

---
+++ src/main/java/scrabble/model/Square.java
@@ -0,0 +1,30 @@
+package scrabble.model;
+
+public class Square {
+	
+	protected char symbol;
+	protected int x;
+	protected int y;
+	protected Tile tile;
+	
+	protected Square(char symbol, int x, int y) {
+		this.tile = null;
+		this.symbol = symbol;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Square(int x, int y) {
+		this('.', x, y);
+	}
+	
+	public  void placeTile(Tile tile) {
+		this.tile = tile;
+	}
+	
+	public  Boolean isBusy() {
+		return this.tile == null;
+	}
+	
+
+}

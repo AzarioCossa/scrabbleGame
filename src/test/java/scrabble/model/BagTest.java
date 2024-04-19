0455d52d7fd39439622669ae01eaccb97c7c8c6c
Louis Bec <louis.0505@protonmail.com>
2024-04-19 16:45:19 +0200

    add BagTest.java

---
+++ src/test/java/scrabble/model/BagTest.java
@@ -0,0 +1,28 @@
+package scrabble.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BagTest {
+	@Test
+	public void testAddTile() {
+		Bag bag = new Bag();
+
+		Tile tile = new Tile(Letters.A);
+		assertTrue(bag.addTile(tile));
+
+		assertTrue(bag.getTiles().contains(tile));
+	}
+	 @Test
+	    public void testDrawTile() {
+	        Bag bag = new Bag();
+	        int initialSize = bag.getTiles().size();
+
+	        Tile drawnTile = bag.drawTile();
+
+	        assertEquals(initialSize - 1, bag.getTiles().size());
+	        assertFalse(bag.getTiles().contains(drawnTile)); 
+	    }
+
+}

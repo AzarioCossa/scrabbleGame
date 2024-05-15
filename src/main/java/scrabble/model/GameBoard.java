0b704b98d7052ce6cf1201466594c6ae73b0160d
AzarioCossa <cossaazario@gmail.com>
2024-05-15 14:19:48 +0200

    add code refactor : integration of BoardConstants.java on GameBoard.java

--- src/main/java/scrabble/model/GameBoard.java
+++ src/main/java/scrabble/model/GameBoard.java
@@ -2,34 +2,21 @@ package scrabble.model;
 
 public class GameBoard {
 
-	private Square[][] squares;
-	private int sizeX;
-	private int sizeY;
+    private Square[][] squares;
 
-	public GameBoard(int x, int y) {
-		this.sizeX = x;
-		this.sizeY = y;
+    public GameBoard() {
 
-		squares = new Square[x][y];
+        squares = new Square[ BoardSizeConstants.DEFAULT_NUM_ROWS][BoardSizeConstants.DEFAULT_NUM_COLUMNS];
 
-		for (int row = 0; row < x; row++) {
-			for (int column = 0; column < y; column++) {
-				squares[row][column] = new Square();
-			}
-		}
-		squares[7][7] = new SquareStar();
-	}
+        for (int row = 0; row < BoardSizeConstants.DEFAULT_NUM_ROWS; row++) {
+            for (int column = 0; column < BoardSizeConstants.DEFAULT_NUM_COLUMNS; column++) {
+                squares[row][column] = new Square();
+            }
+        }
+        squares[7][7] = new SquareStar();
+    }
 
-	public Square[][] getSquares() {
-		return squares;
-	}
-
-	public int getSizeX() {
-		return sizeX;
-	}
-
-	public int getSizeY() {
-		return sizeY;
-	}
-	
+    public Square[][] getSquares() {
+        return squares;
+    }
 }

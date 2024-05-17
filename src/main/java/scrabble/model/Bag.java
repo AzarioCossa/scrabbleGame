27a2747a90b96dab5a8827b17311d5293da6922a
Ilyas Boukhari <ilyasboukh87@gmail.com>
2024-05-17 16:42:04 +0200

    implementation of method addTile in Bag

--- src/main/java/scrabble/model/Bag.java
+++ src/main/java/scrabble/model/Bag.java
@@ -3,11 +3,14 @@ package scrabble.model;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import scrabble.model.utils.BagIsFullException;
 import scrabble.model.utils.EmptyBagException;
+import scrabble.model.utils.RackIsFullException;
 
 public class Bag {
     private ArrayList<Tile> tiles;
-
+    public final static Integer LIMIT_BAG_CAPACITY = 102;
+    
     public Bag() {
         tiles = new ArrayList<>();
         generateTiles();
@@ -29,7 +32,12 @@ public class Bag {
 		throw new EmptyBagException("You can't draw a tile when the bag is empty");
     }
 
-	
+	public Boolean addTile(Tile tile) throws BagIsFullException{
+		if (this.tiles.size() >= LIMIT_BAG_CAPACITY) {
+			throw new BagIsFullException("Le bag est plein !");
+		}
+		return this.tiles.add(tile);
+	}
     private void generateTiles() {
         for (FrenchLetters letter : FrenchLetters.values()) {
             for (int i = 0; i < letter.getQuantity(); i++) {

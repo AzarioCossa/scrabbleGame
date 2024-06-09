b9edf893eab1791b3717134e0c97e0c96b62e059
Louis Bec <louis.0505@protonmail.com>
2024-06-09 18:46:47 +0200

    adding somme test for the excepytion TilePlacement

--- src/test/java/scrabble/model/GameBoardTest.java
+++ src/test/java/scrabble/model/GameBoardTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import scrabble.model.utils.TilePlacementException;
+
 class GameBoardTest {
 
 	@Test
@@ -11,9 +13,20 @@ class GameBoardTest {
 	
 		GameBoard gameBoard = new GameBoard();
 
-		assertTrue(gameBoard.getSquares()[1][1] instanceof Square);
+        assertTrue(gameBoard.getSquares()[7][7].getSquareType() == SquareType.CENTRAL);
+        assertTrue(gameBoard.getSquares()[7][8].getSquareType() == SquareType.STANDARD);
 
 		
 	}
 
+	 @Test
+	    void testPlaceTileOutsideBounds() {
+	        GameBoard gameBoard = new GameBoard();
+	        Tile tile = new Tile(FrenchLetters.A);
+
+	        assertThrows(TilePlacementException.class, () -> gameBoard.placeTileGameBoard(tile, -1, 0));
+	        assertThrows(TilePlacementException.class, () -> gameBoard.placeTileGameBoard(tile, 0, -1));
+	        assertThrows(TilePlacementException.class, () -> gameBoard.placeTileGameBoard(tile, BoardSizeConstants.BOARD_SIZE, 0));
+	        assertThrows(TilePlacementException.class, () -> gameBoard.placeTileGameBoard(tile, 0, BoardSizeConstants.BOARD_SIZE));
+	    }
 }

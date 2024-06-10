ddc00cf4d6848ca27d8b4c597dfe08fbbafef387
AzarioCossa <cossaazario@gmail.com>
2024-06-10 11:24:56 +0200

    refactor add comments and structure to AnimationManager.java

--- src/main/java/scrabble/util/AnimationManager.java
+++ src/main/java/scrabble/util/AnimationManager.java
@@ -6,29 +6,44 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.util.Duration;
 
+/**
+ * The AnimationManager class provides utility methods for managing animations in the Scrabble game.
+ */
 public class AnimationManager {
-	private AnimationManager() {
-	}
 
-	
+    /**
+     * Private constructor to prevent instantiation of the class.
+     */
+    private AnimationManager() {
+    }
 
-	public static void animateFade(StackPane stackPane) {
+    /**
+     * Animates the fade effect on a StackPane.
+     * 
+     * @param stackPane the StackPane to animate
+     */
+    public static void animateFade(StackPane stackPane) {
 
-		FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), stackPane);
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(2), stackPane);
 
-		fadeTransition.setFromValue(0);
-		fadeTransition.setToValue(1);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
 
-		fadeTransition.play();
-	}
+        fadeTransition.play();
+    }
 
-	public static void addShadowOnHover(StackPane stackPane, ImageView imageView) {
-		ColorAdjust colorAdjust = new ColorAdjust();
-		colorAdjust.setBrightness(-0.2);
+    /**
+     * Adds a shadow effect to an ImageView when hovered over a StackPane.
+     * 
+     * @param stackPane the StackPane to apply the hover effect
+     * @param imageView the ImageView to add the shadow effect
+     */
+    public static void addShadowOnHover(StackPane stackPane, ImageView imageView) {
+        ColorAdjust colorAdjust = new ColorAdjust();
+        colorAdjust.setBrightness(-0.2);
 
-		stackPane.setOnMouseEntered(event -> imageView.setEffect(colorAdjust));
+        stackPane.setOnMouseEntered(event -> imageView.setEffect(colorAdjust));
 
-		stackPane.setOnMouseExited(event -> imageView.setEffect(null));
-
-	}
+        stackPane.setOnMouseExited(event -> imageView.setEffect(null));
+    }
 }

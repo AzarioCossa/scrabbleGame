0e2bdf15b8c06c0e95a2870ebbab11df11706326
AzarioCossa <cossaazario@gmail.com>
2024-06-10 11:20:17 +0200

    refactor add commments and structure to EmptyBagException.java

--- src/main/java/scrabble/model/utils/EmptyBagException.java
+++ src/main/java/scrabble/model/utils/EmptyBagException.java
@@ -1,11 +1,18 @@
 package scrabble.model.utils;
 
+/**
+ * The EmptyBagException class represents an exception that is thrown when attempting to draw a tile from an empty bag.
+ */
 public class EmptyBagException extends Exception {
-	private static final long serialVersionUID = 1L;
 
-	public EmptyBagException(String message) {
+    private static final long serialVersionUID = 1L;
 
-		super(message);
-	}
-
+    /**
+     * Constructs a new EmptyBagException with the specified detail message.
+     * 
+     * @param message the detail message
+     */
+    public EmptyBagException(String message) {
+        super(message);
+    }
 }

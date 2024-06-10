50dd72a2b1d83a10a514d3009f8a6be2ff15d1ea
AzarioCossa <cossaazario@gmail.com>
2024-06-10 11:01:46 +0200

    add refactor  and structure to Direction.java

--- src/main/java/scrabble/model/Direction.java
+++ src/main/java/scrabble/model/Direction.java
@@ -1,20 +1,51 @@
 package scrabble.model;
 
+/**
+ * The Direction enum represents the four possible directions for placing tiles in the Scrabble game.
+ * Each direction has a corresponding string representation.
+ */
 public enum Direction {
 
-	UP("up"),
-	RIGHT("right"),
-	DOWN("down"),
-	LEFT("left");
+    /**
+     * Represents the upward direction.
+     */
+    UP("up"),
 
-	
-	private final String stringName;
-	
-	private Direction(String stringName) {
-		this.stringName = stringName;
-	}
+    /**
+     * Represents the rightward direction.
+     */
+    RIGHT("right"),
 
-	public String getStringName() {
-		return stringName;
-	}
+    /**
+     * Represents the downward direction.
+     */
+    DOWN("down"),
+
+    /**
+     * Represents the leftward direction.
+     */
+    LEFT("left");
+
+    /**
+     * The string representation of the direction.
+     */
+    private final String stringName;
+
+    /**
+     * Constructs a Direction with the specified string name.
+     *
+     * @param stringName the string representation of the direction
+     */
+    private Direction(String stringName) {
+        this.stringName = stringName;
+    }
+
+    /**
+     * Returns the string representation of the direction.
+     *
+     * @return the string representation of the direction
+     */
+    public String getStringName() {
+        return stringName;
+    }
 }
